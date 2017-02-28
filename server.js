5bfaa3b8247602dc11fd6cb2499a4d09c5cb17a7
Dana Kulp <dkulp23@gmail.com>
2017-02-28 15:41:28 -0800

    Required in modules to server file and set up middleware flow.

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 'use strict';
 
+const morgan = require('morgan');
 const express = require('express');
 const debug = require('debug')('family:server');
-const PORT = process.env.PORT || 3000;
 
+const familyRouter = require('./route/family-router.js');
+const cors = require('./lib/cors-middleware.js');
+const errors = require('./lib/error-middleware.js');
+
+const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(morgan('dev'));
+app.use(cors);
+app.use(familyRouter);
+app.use(errors);
 
 app.listen(PORT, () => {
   debug(`Server's up on port: ${PORT}`);

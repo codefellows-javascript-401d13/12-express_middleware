74521b4d4dc1015d2ab4a75c48810ee7de56a234
Dana Kulp <dkulp23@gmail.com>
2017-02-28 20:05:58 -0800

    Working on tests for PUT route. Realized that my POST route wasn't working because I forgot the parens after JSON on body parser.

--- route/family-router.js
+++ route/family-router.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const Router = require('express').Router;
-const jsonParser = require('body-parser').json;
+const jsonParser = require('body-parser').json();
 const debug = require('debug')('family:routes');
 const Family = require('../model/family.js');
 const familyRouter = new Router();
@@ -20,6 +20,22 @@ familyRouter.get('/api/family/:id', function(req, res, next) {
   Family.fetchFamily(req.params.id)
   .then( family => res.json(family))
   .catch( err => next(err));
+});
+
+familyRouter.get('/api/family', function(req, res, next) {
+  debug('GET: api/family');
+
+  Family.fetchIDs()
+  .then( ids => res.json(ids))
+  .catch (err => next(err));
+});
+
+familyRouter.put('/api/family/:id', function(req, res, next) {
+  debug('PUT: api/family');
+
+  Family.updateFamily(req.query.id, req.body)
+  .then( family => res.json(family))
+  .catch(next);
 });
 
 module.exports = familyRouter;
--- test/family-route-test.js
+++ test/family-route-test.js
@@ -13,39 +13,103 @@ const exampleFamily = {
 };
 
 describe('Family Routes', function() {
+
   describe('GET: /api/family', function() {
-    before( done => {
-      Family.createFamily(exampleFamily)
-      .then( family => {
-        this.tempFamily = family;
-        done();
-      })
-      .catch( err => done(err));
-    });
+    describe('with a valid id', function() {
+      before( done => {
+        Family.createFamily(exampleFamily)
+        .then( family => {
+          this.tempFamily = family;
+          done();
+        })
+        .catch( err => done(err));
+      });
 
-    after( done => {
-      Family.deleteFamily(this.tempFamily.id)
-      .then( () => done())
-      .catch( err => done(err));
-    });
+      after( done => {
+        Family.deleteFamily(this.tempFamily.id)
+        .then( () => done())
+        .catch( err => done(err));
+      });
 
-    it('should return a note', done => {
-      request.get(`${url}/api/family/${this.tempFamily.id}`)
-      .end((err, res) => {
-        if (err) return done(err);
-        expect(res.status).to.equal(200);
-        expect(res.body.id).to.equal(this.tempFamily.id);
-        expect(res.body.name).to.equal(this.tempFamily.name);
-        expect(res.body.relation).to.equal(this.tempFamily.relation);
-        done();
+      it('should return a note', done => {
+        request.get(`${url}/api/family/${this.tempFamily.id}`)
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.id).to.equal(this.tempFamily.id);
+          expect(res.body.name).to.equal(this.tempFamily.name);
+          expect(res.body.relation).to.equal(this.tempFamily.relation);
+          done();
+        });
+      });
+
+      describe('with an invalid id', function() {
+        it('should return a 404 error', done => {
+          request.get(`${url}/api/family/123456789`)
+          .end((err, res) => {
+            expect(res.status).to.equal(404);
+            done();
+          });
+        });
       });
     });
+  });
 
-    describe('with an invalid id', function() {
-      it('should return a 404 error', done => {
-        request.get(`${url}/api/family/123456789`)
+  describe('POST: /api/family', function() {
+    describe('with a valid body', function() {
+      after( done => {
+        if (this.tempFamily) {
+          Family.deleteFamily(this.tempFamily.id)
+          .then( () => done())
+          .catch( err => done(err));
+        }
+      });
+
+      it('should return a note', done => {
+        request.post(`${url}/api/family`)
+        .send(exampleFamily)
         .end((err, res) => {
-          expect(res.status).to.equal(404);
+          if (err) return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.name).to.equal(exampleFamily.name);
+          expect(res.body.relation).to.equal(exampleFamily.relation);
+          this.tempFamily = res.body;
+          done();
+        });
+      });
+    });
+  });
+
+  describe('PUT: /api/family', function() {
+    describe('with a valid id and body', function() {
+      before( done => {
+        Family.createFamily(exampleFamily)
+        .then( family => {
+          this.tempFamily = family;
+          done();
+        })
+        .catch( err => done(err));
+      });
+
+      after( done => {
+        if (this.tempFamily) {
+          Family.deleteFamily(this.tempFamily.id)
+          .then( () => done())
+          .catch(done);
+        }
+      });
+
+      it('should return an updated family', done => {
+        let updatedFamily = { name: 'Jill', relation: 'mom' };
+        request.put(`${url}/api/family/${this.tempFamily.id}`)
+        .send(updatedFamily)
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.id).to.equal(this.tempFamily.id);
+          for (var prop in updatedFamily) {
+            expect(res.body[prop]).to.equal(updatedFamily[prop]);
+          }
           done();
         });
       });
